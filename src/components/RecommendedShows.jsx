3e4105613a2cd66b6107b7869164893111333381
Kuhu Bhatnagar <kuhubhatnagar19@gmail.com>
2024-04-07 23:39:27 +0530

    single responsibility functionality on shows

--- src/components/RecommendedShows.jsx
+++ src/components/RecommendedShows.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import {showsURL} from '../utils/Constants'; 
+import EventCard from "./EventCard";
 
 const RecommendedShows = () => {
     const [data,setData] = useState([]);
@@ -13,32 +14,11 @@ const RecommendedShows = () => {
         setData(jsonData.events);
     }
 
-    const ShowData = ({show}) =>{
-         const id = show.imgUrl.split('/')[5]
-         const url = 'https://drive.google.com/thumbnail?id='
-
-         const dateString = show.date.slice(0,10);
-
-         var date = new Date(dateString);
-         var monthNames = ["January", "February", "March", "April", "May", "June",
-                  "July", "August", "September", "October", "November", "December"];
-
-         const formattedDate = date.getDate() + " " + monthNames[date.getMonth()] + ", " + date.getFullYear()
-         console.log(formattedDate);
-         return <div className="event-data">
-            <img className='event-image'src={url+id} alt="image" />
-            <div className="event-details">
-                <p><b>{show.eventName}</b></p>
-                <p>{formattedDate}</p>
-                <p>{show.cityName}</p>
-                <p>{show.weather}</p>
-            </div>
-         </div>
-    } 
+    
     return <div className="show-container">
         <h2>Recommended Events &#x2192;</h2>
         <div className="wrapper">
-        {data.map((show,index) => <ShowData key={index} show={show}/>)}
+        {data.map((show,index) => <EventCard key={index} event={show}/>)}
         </div>
     </div>
 }
