02f4665c6ce14b2782a90a19283c105d7be1c530
Kuhu Bhatnagar <kuhubhatnagar19@gmail.com>
2024-04-08 23:20:27 +0530

    add icons for navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,37 @@
 import '../index.css';
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import {faBars} from '@awesome.me/kit-KIT_CODE/icons/classic/solid'
+import { GiHamburgerMenu } from "react-icons/gi";
+import { CiSearch } from "react-icons/ci";
+import { FaHeart } from "react-icons/fa6";
+import { FaLocationDot } from "react-icons/fa6";
+import { MdKeyboardArrowRight } from "react-icons/md";
+
 const Header = () => {
   return (  
     <div className='header'>
       <div className="navbar">
         <h3 className='title'>BookUsNow</h3>
-        <button className='categories-btn'>Categories</button>
-        <input className='searchbar' type="text" placeholder='DJI Phantom'/>
-        <p>Favourites</p>
+        <div className="nav-items">
+        <button className='categories-btn icons'>
+          <GiHamburgerMenu className='categories-icon'/>
+          Categories
+        </button>
+        <div className="search icons">
+          <input className='search-bar'type="text" placeholder='DJI Phantom'/>
+          <CiSearch className='search-icon'/>
+        </div>
+        <div className="favourites icons">
+          <FaHeart />
+          Favourites
+        </div>
         <button className='sign-in-btn'>Sign in </button>
         </div>
+        </div>
       <div className="events">
-        <p>Mumbai,India</p>
+        <div className="location icons">
+          <FaLocationDot />
+          Mumbai,India
+          <MdKeyboardArrowRight />
+        </div>
         <ul className="event-types">
           <li>Live Shows</li>
           <li>Streams</li>
